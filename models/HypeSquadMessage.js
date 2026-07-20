70d3426436d0e4a816b901a5efd7f5e0445a14f4
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-07-20 11:55:13 +0700

    Delete models directory

--- models/HypeSquadMessage.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const hypeSquadSchema = new mongoose.Schema({
-    guildId: String,
-    channelId: String,
-    messageId: String
-});
-
-module.exports = mongoose.model('HypeSquadMessage', hypeSquadSchema);
