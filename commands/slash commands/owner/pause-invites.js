db0f64d0bbe72ed7e706160b12f358cb672137c6
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-07-20 17:50:55 +0700

    Update pause-invites.js

--- commands/slash commands/owner/pause-invites.js
+++ commands/slash commands/owner/pause-invites.js
@@ -5,6 +5,7 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('pause-invites')
         .setDescription('Manage the pause server invites')
+        .setDMPermission(false)
         // Keeps it hidden in the menu from non-admins, but we will still manually check below
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild) 
         
