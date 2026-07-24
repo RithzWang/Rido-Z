d43261eb29fc0d9c8160e7caaadab247d6b605d9
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-07-24 11:24:18 +0700

    Update TranslatorSchema.js

--- schema/TranslatorSchema.js
+++ schema/TranslatorSchema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const translatorSchema = new mongoose.Schema({
     guildId: { type: String, required: true },
     channelId: { type: String, required: true, unique: true },
-    language: { type: String, required: true, enum: ['english', 'spanish', 'arabic', 'thai', 'bilingual'] }
+    language: { type: String, required: true, enum: ['english', 'spanish', 'arabic', 'thai'] }
 });
 
 module.exports = mongoose.model('Translator', translatorSchema);
