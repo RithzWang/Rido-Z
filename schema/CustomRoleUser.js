7bf0c293350934011de61c1f558263966b4a40dc
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-09-22 19:08:30 +0700

    Update CustomRoleUser.js

--- schema/CustomRoleUser.js
+++ schema/CustomRoleUser.js
@@ -3,7 +3,10 @@ const { Schema, model } = require('mongoose');
 const userRoleSchema = new Schema({
     guildId: { type: String, required: true },
     userId: { type: String, required: true },
-    roleId: { type: String, required: true }
+    roleId: { type: String, required: true },
+    style: { type: String, enum: ['solid', 'gradient'], default: 'solid' },
+    primaryColor: { type: String, default: null },
+    secondaryColor: { type: String, default: null }
 });
 
 module.exports = model('CustomRoleUser', userRoleSchema);
