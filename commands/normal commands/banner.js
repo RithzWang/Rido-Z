50a056eb2189d2d36139fca3fbcbad364eef0dfb
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-09-22 17:41:32 +0700

    Update banner.js

--- commands/normal commands/banner.js
+++ commands/normal commands/banner.js
@@ -61,7 +61,7 @@ module.exports = {
             // 3. Builder
             const buildMessagePayload = async (isShowingGlobal, disableToggle = false) => {
                 const currentImageUrl = isShowingGlobal ? globalBanner : displayBanner;
-                const titleText = isShowingGlobal ? `## Profile Banner` : `## Per-server Profile Banner`;
+                const titleText = isShowingGlobal ? `## <:image:1551901191124947026> Profile Banner` : `## <:image:1551901191124947026> Per-server Profile Banner`;
                 
                 // 👇 UPDATED: Added `targetUser.username` in backticks
                 const bodyText = isShowingGlobal 
