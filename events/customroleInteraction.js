a2298d5c693db3dc745e304eddaf028908ff0643
RithzWang <101114513+RithzWang@users.noreply.github.com>
2026-09-23 23:54:30 +0700

    Update customroleInteraction.js

--- events/customroleInteraction.js
+++ events/customroleInteraction.js
@@ -224,10 +224,10 @@ module.exports = {
 
             // Format color texts appropriately based on Solid vs Gradient
             const isGradient = userRoleData.style === 'gradient';
-            let formattedColorText = `_Colour_: \`${userRoleData.primaryColor.toUpperCase()}\``;
+            let formattedColorText = `_Colour_: \`#${userRoleData.primaryColor.toUpperCase()}\``;
             
             if (isGradient && userRoleData.secondaryColor) {
-                formattedColorText = `_Primary Colour_: \`${userRoleData.primaryColor.toUpperCase()}\`\n_Secondary Colour_: \`${userRoleData.secondaryColor.toUpperCase()}\``;
+                formattedColorText = `_Primary Colour_: \`#${userRoleData.primaryColor.toUpperCase()}\`\n_Secondary Colour_: \`#${userRoleData.secondaryColor.toUpperCase()}\``;
             }
 
             const confirmationComponents = [
